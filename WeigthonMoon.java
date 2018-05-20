275b8c310d757c1e4d76643848c8c161bc71eece
8AngryKitty8 <ketol2468@gmail.com>
2018-05-20 20:14:23 +0400

    Readme has been added

---
+++ WeigthonMoon.java
@@ -0,0 +1,9 @@
+/*Сила тяжести на луне составляет коло 17% земной.
+Напишите программу, которая вычисляла бы ваш вес на Луне*/
+class WeigthonMoon {
+public static void main (String args []){
+	int weigth=60;
+	double weigth1=weigth*0.17;
+	System.out.println ("Вес на луне составляет "+weigth1+" кг");
+}
+}
